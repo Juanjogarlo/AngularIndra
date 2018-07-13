e5f376f864646a347dfc6fcb63acba28ffcc3c76
formacion <formacion@ANSFORM35.indra.es>
2018-07-13 11:20:04 +0200

    Conexión a servidor

--- src/app/personas/personas.service.ts
+++ src/app/personas/personas.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LoggerService } from '../../my-core';
 import { NotificationService } from '../services/notification.service';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
